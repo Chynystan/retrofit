6b4fcea1465b7798773a70fb9a8d6cddf1f5d6f8
TechLine <azizsuerkulov2016@gmail.com>
2022-07-02 21:38:24 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/retrofit/retrofit/PixabayApi.java
@@ -0,0 +1,18 @@
+package com.geektech.retrofit.retrofit;
+
+
+
+import com.geektech.retrofit.model.PixaModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PixabayApi {
+
+    @GET("api/?key=28087804-7ca6d07127942d844d74ba2bc")
+    Call<PixaModel> getImage(@Query("q") String keyWord,
+                             @Query("per_page") int perPage,
+                             @Query("page") int page);
+    
+}
